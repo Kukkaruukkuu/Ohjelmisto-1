66bd4bfb88f711fcef06d9354b72628a6ea1b573
Milla <millsu@metropolia.fi>
2026-09-04 16:51:51 +0300

    luotu projekti kansia jo eka tiedosto

---
+++ Peliprojekti/readme.md
@@ -0,0 +1,8 @@
+# Ohjelmisto 1 - Peliprojekti
+
+**Milla Sundell**
+
+Luotu tiedosto joka kysyy nimen ja iän
+
+
+
---
+++ Peliprojekti/tiedot.py
@@ -0,0 +1,7 @@
+#Tee kansioon ohjelma, joka kysyy pelaajan nimen ja iän, tallentaa nämä muuttujiin ja 
+# tulostaa konsoliin.
+
+
+user_nimi = input("Mikä nimesi on: ")
+user_ika = input("Kuinka vanha olet: ")
+print(user_nimi + user_ika)
